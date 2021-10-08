d6fe60cf65164ccea6daafec8d5f414a97dde80b
Chell7 <86483154+Chell7@users.noreply.github.com>
2021-10-07 22:11:33 -0400

    Update chal1.py

--- chal1.py
+++ chal1.py
@@ -1,61 +1,141 @@
-#CHALLENGES 1-11
-#1
-name = input('Enter your first name')
-print('Hello',name)
+#CHALLENGES 60-68
+#60
+import turtle 
+for i in range (4) :
+    turtle.forward(100)
+    turtle.left(90)
 
-#2
-name = input('Enter your first name')
-name1 = input('Enter your surname')
-print('Hello',name, name1)
+turtle.exitonclick()
 
-#3
-print('What do you call a bear with no teeth ?\n A gummy bear !')
+#61
+import turtle 
+for i in range(3) :
+    turtle.left(120)
+    turtle.forward(100)
 
-#4
-num1 = int(input('Enter the first number'))
-num2 = int(input('Enter the second number'))
-answer= num1+num2
-print('The answer is', answer)
+turtle.exitonclick()
 
-#5
-num1 = int(input('Enter the first number'))
-num2 = int(input('Enter the second number'))
-num3 = int(input('Enter the third number'))
-answer= (num1+num2)*num3
-print('The answer is', answer)
+#62
+import turtle 
+for i in range(360) :
+        turtle.forward(1)
+        turtle.right(1)
 
-#6
-totalparts= int(input('Enter the first number'))
-partseaten= int(input('Enter the second number'))
-partsleft= totalparts-partseaten
-print('The parts of pizza left are', partsleft)
+turtle.exitonclick()
 
-#7
-name= input('What is your name ?')
-age= int(input('How old are you?'))
-newage= age + 1
-print (name, ', next birthday you will be', newage)
+#63
+import turtle 
+turtle.color('green')
+turtle.begin_fill()
+for i in range (4) :
+    turtle.forward(100)
+    turtle.left(90)
+turtle.penup()
+turtle.end_fill()
+turtle.forward(100)
+    
+turtle.pendown
+turtle.color('yellow')
+turtle.begin_fill()
+for i in range (4) :
+    turtle.forward(100)
+    turtle.left(90)
+turtle.penup()
+turtle.end_fill()
+turtle.forward(100)
 
-#8
-bill= int(input('Enter the amount of the bill'))
-diners= int(input('Enter the quantity of persons'))
-billunity= bill/diners
-print('Each person must pay', billunity )
+turtle.pendown
+turtle.color('black')
+turtle.begin_fill()
+for i in range (4) :
+    turtle.forward(100)
+    turtle.left(90)
+turtle.end_fill()
 
-#9
-numberofdays= int(input('Enter the quantity of days'))
-hours= numberofdays *24
-minutes= hours*60
-seconds= minutes*60
-print('In days : \n','There are', hours, 'hours ;' , minutes,'minutes ;',  seconds ,'seconds .')
+turtle.exitonclick()
 
-#1o
-kg= int(input('Enter the weight in kilogram: '))
-pounds= 2.204*kg
-print('The result is', pounds, 'pounds')
+#64
+import turtle 
+for i in range (5) :
+    turtle.forward(100)
+    turtle.right(144)
 
-#11
-num1=int(input('Enter a number over 100 : '))
-num2=int(input('Enter a number under 100 : '))
-answer= num1//num2
-print('The smaller number goes into the larger number', answer, 'times.')
+turtle.exitonclick()
+
+#65
+import turtle
+
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+turtle.forward(75)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(75)
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+turtle.forward(75)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(45)
+turtle.left(180)
+turtle.forward(45)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(75)
+
+turtle.hideturtle()
+
+turtle.exitonclick()
+
+#66
+import turtle
+import random
+
+turtle.pensize(4)
+
+for i in range (0,8) :
+    turtle.color(random.choice(['red','orange','green', 'blue', 'black', 'pink']))
+    turtle.forward(50)
+    turtle.right(45)
+
+turtle.exitonclick()
+
+#67
+import random
+import turtle
+
+for x in range (0,10) :
+    for i in range (0, 8) :
+        turtle.forward(50)
+        turtle.right(45)
+    turtle.right(36)
+
+turtle.hideturtle()
+
+turtle.exitonclick()
+
+#68
+import turtle
+import random
+
+lines= random.randint(5,20)
+
+for y in range (0,lines) :
+    length = random.randint(25, 100)
+    rotate= random.randint(1,365)
+    turtle.forward(length)
+    turtle.right(rotate)
+
+turtle.exitonclick
